c3c023c5179af3dbbc6afa3f68147b1005788bb7
Savan Asodariya <sasodariya@eedgetechnology.com>
2023-07-08 00:21:05 -0400

    WIP

---
+++ ios/PicaggoPoc/AWSRNS3TransferUtility.h
@@ -0,0 +1,10 @@
+//
+//  AWSRNS3TransferUtility.h
+//  PicaggoPoc
+//
+//  Created by user218041 on 7/8/23.
+//
+
+#import <React/RCTBridgeModule.h>
+@interface AWSRNS3TransferUtility : NSObject <RCTBridgeModule>
+@end
---
+++ ios/PicaggoPoc/AWSRNS3TransferUtility.m
@@ -0,0 +1,15 @@
+//
+//  AWSRNS3TransferUtility.m
+//  PicaggoPoc
+//
+//  Created by user218041 on 7/8/23.
+//
+
+#import "AWSRNS3TransferUtility.h"
+
+@implementation AWSRNS3TransferUtility
+
+RCT_EXPORT_MODULE(AWSRNS3TransferUtility);
+
+@end
+	
